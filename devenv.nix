0d0998561315732dad0237de31c8fc90c5b36794
Rick Henry <rickhenry@rickhenry.dev>
2023-10-18 14:48:19 -0400

    initial commit
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

---
+++ devenv.nix
@@ -0,0 +1,46 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [
+    pkgs.git
+    pkgs.nodejs
+    pkgs.bun
+  ];
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = "echo hello from $GREET";
+
+
+  certificates = [
+    "practicing.localhost"
+  ];
+
+  services.caddy = {
+    enable = true;
+    virtualHosts."practicing.localhost".extraConfig = ''
+      reverse_proxy :5100
+    '';
+  };
+
+  languages = {
+    javascript.enable = true;
+  };
+
+  # https://devenv.sh/languages/
+  # languages.nix.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+  processes = {
+    next.exec = "npm run dev";
+  };
+}
---
+++ src/server/auth.ts
@@ -0,0 +1,231 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { type GetServerSidePropsContext } from "next";
+import {
+  getServerSession,
+  type DefaultSession,
+  type NextAuthOptions,
+  type Session,
+  type RequestInternal,
+} from "next-auth";
+import EmailProvider from "next-auth/providers/email";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+import { env } from "~/env.mjs";
+import { db } from "~/server/db";
+import { domain, getChallenge, rpID } from "~/server/webauthn";
+import { verifyAuthenticationResponse } from "@simplewebauthn/server";
+import { type JWT } from "next-auth/jwt";
+
+/**
+ * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
+ * object and keep type safety.
+ *
+ * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
+ */
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: DefaultSession["user"] & {
+      id: string;
+      // ...other properties
+      // role: UserRole;
+    };
+  }
+
+  // interface User {
+  //   // ...other properties
+  //   // role: UserRole;
+  // }
+}
+
+/**
+ * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
+ *
+ * @see https://next-auth.js.org/configuration/options
+ */
+export const authOptions: NextAuthOptions = {
+  callbacks: {
+    session: ({ session, token }: { session: Session; token: JWT }) => {
+      if (token?.id && typeof token?.id === "string") {
+        session = {
+          ...session,
+          user: {
+            ...session.user,
+            id: token.id,
+          },
+        };
+      }
+      return session;
+    },
+    jwt: ({ token, user }) => {
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+      }
+      return token;
+    },
+  },
+  adapter: PrismaAdapter(db),
+  secret: env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    CredentialsProvider({
+      name: "webauthn",
+      credentials: {},
+      async authorize(_cred, req) {
+        if (!req.body) return null;
+        const {
+          id,
+          rawId,
+          type,
+          clientDataJSON,
+          authenticatorData,
+          signature,
+          userHandle,
+          clientExtensionResults,
+        } = req.body;
+
+        const response = {
+          id,
+          rawId,
+          type,
+          response: {
+            clientDataJSON,
+            authenticatorData,
+            signature,
+            userHandle,
+          },
+          clientExtensionResults,
+        };
+        // console.log("response", response);
+
+        const authenticator = await db.credential.findFirst({
+          where: {
+            credentialID: response.id,
+          },
+        });
+
+        if (!authenticator) {
+          console.log("authenticator not found");
+          return null;
+        }
+        const expectedChallenge = await getChallenge(authenticator.userId);
+        if (!expectedChallenge) {
+          return null;
+        }
+        try {
+          const { verified, authenticationInfo } =
+            await verifyAuthenticationResponse({
+              response,
+              expectedChallenge,
+              expectedOrigin: domain,
+              expectedRPID: rpID,
+              authenticator: {
+                credentialPublicKey: authenticator.credentialPublicKey,
+                counter: authenticator.counter,
+                credentialID: Buffer.from(
+                  authenticator.credentialID,
+                  "base64url",
+                ),
+              },
+            });
+          if (!verified || !authenticationInfo) {
+            console.log("failed to verify");
+            return null;
+          }
+
+          return await db.user.findFirst({
+            where: {
+              id: authenticator.userId,
+            },
+          });
+        } catch (err) {
+          console.log(err);
+          return null;
+        }
+      },
+    }),
+
+    EmailProvider({
+      server: {
+        host: env.EMAIL_SERVER_HOST,
+        port: env.EMAIL_SERVER_PORT,
+        auth: {
+          user: env.EMAIL_SERVER_USER,
+          pass: env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: env.EMAIL_FROM,
+    }),
+  ],
+  pages: {
+    signIn: "/auth/signin",
+  },
+};
+
+function getWebauthnBody(req: RequestInternal) {
+  if (!req.body) {
+    throw new Error("Missing body");
+  }
+  const {
+    id,
+    rawId,
+    type,
+    clientDataJSON,
+    authenticatorData,
+    signature,
+    userHandle,
+    clientExtensionResults,
+  } = req.body;
+
+  if (!id || typeof id !== "string") {
+    throw new Error("Missing id");
+  }
+
+  if (!rawId || typeof rawId !== "string") {
+    throw new Error("Missing rawId");
+  }
+  if (!type || typeof type !== "string") {
+    throw new Error("Missing type");
+  }
+  if (!clientDataJSON || typeof clientDataJSON !== "string") {
+    throw new Error("Missing clientDataJSON");
+  }
+  if (!authenticatorData || typeof authenticatorData !== "string") {
+    throw new Error("Missing authenticatorData");
+  }
+  if (!signature || typeof signature !== "string") {
+    throw new Error("Missing signature");
+  }
+  if (!userHandle || typeof userHandle !== "string") {
+    throw new Error("Missing userHandle");
+  }
+  if (!clientExtensionResults || typeof clientExtensionResults !== "object") {
+    throw new Error("Missing clientExtensionResults");
+  }
+
+  return {
+    id,
+    rawId,
+    type,
+    clientDataJSON,
+    authenticatorData,
+    signature,
+    userHandle,
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    clientExtensionResults,
+  };
+}
+
+/**
+ * Wrapper for `getServerSession` so that you don't need to import the `authOptions` in every file.
+ *
+ * @see https://next-auth.js.org/configuration/nextjs
+ */
+export const getServerAuthSession = (ctx: {
+  req: GetServerSidePropsContext["req"];
+  res: GetServerSidePropsContext["res"];
+}) => {
+  return getServerSession(ctx.req, ctx.res, authOptions);
+};
