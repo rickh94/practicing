d8be56ece9e64ab550957bc3a30d496542d8cc12
Rick Henry <rickhenry@rickhenry.dev>
2023-11-19 22:34:49 -0500

    urgh
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- src/app/api/clean-files/route.ts
+++ src/app/api/clean-files/route.ts
@@ -1,11 +1,19 @@
 import { client } from "~/server/db";
 import { utapi } from "~/server/uploadthing";
 
+export const runtime = "edge";
+
 export async function GET() {
+  if (!process.env.VERCEL || process.env.CI) {
+    return new Response("not allowed", { status: 403 });
+  }
   const ufFilesPromise = utapi.listFiles();
   const spotRecordUrls = await client.execute(
     'SELECT audioPromptUrl, imagePromptUrl from spot WHERE audioprompturl LIKE "%utfs.io%" OR imageprompturl LIKE "%utfs.io%"',
   );
+  if (spotRecordUrls.rows.length === 0) {
+    return new Response("no spot records found", { status: 404 });
+  }
   const keepingKeys = [];
   for (const row of spotRecordUrls.rows) {
     const { audioPromptUrl, imagePromptUrl } = row;
